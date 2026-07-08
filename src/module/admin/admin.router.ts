cd5f5d7e9a6c430c141fd9d57b37ce1ed046d610
Abdur123-drop <mohammedrahim516@gmail.com>
2026-07-08 18:50:38 +0600

    all validation done

--- src/module/admin/admin.router.ts
+++ src/module/admin/admin.router.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { auth } from "../../middleware/auth";
 import { Role } from "../../../generated/prisma/enums";
 import { adminController } from "./admin.controller";
+import { validateRequest } from "../../middleware/validateRequest";
 
 const router = Router()
 
 router.get("/users", auth(Role.ADMIN), adminController.getAllUser)
 router.patch("/users/:id", auth(Role.ADMIN), adminController.updateUserStatus)
 router.get("/gear", auth(Role.ADMIN), adminController.getAllGear)
-router.get("/rentals", auth(Role.ADMIN), adminController.getAllRental)
+router.get("/rentals", auth(Role.ADMIN),  adminController.getAllRental)
 
 export const adminRouter = router;
